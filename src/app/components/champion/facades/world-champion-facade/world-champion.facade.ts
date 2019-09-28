23753e681e556f1cd1c05c694fb79cf2a777ebab
Henry Oforeh <henryoforeh@gmail.com>
2019-09-28 08:12:37 +0200

    add comment to world-champion-facade

--- src/app/components/champion/facades/world-champion-facade/world-champion.facade.ts
+++ src/app/components/champion/facades/world-champion-facade/world-champion.facade.ts
@@ -4,15 +4,24 @@ import { Observable } from 'rxjs/internal/Observable';
 import { of } from 'rxjs';
 import { Champion } from '../../models/champion.model';
 
+/* *
+ *  This facade is used to abstract the business logic of normalizing response payload data
+ *  from the restful service
+ * */
 @Injectable()
 export class WorldChampionFacade {
-
   constructor(private worldChampionService: WorldChampionApiService) {}
 
   listChampions(): Observable<Array<Champion>> {
+    // declare local variable of array type champion
     const champions: Array<Champion> = [];
+    /* *
+     *  invoke get method of WorldChampionApiService api service and subscribe.
+     *  this call is safe because httpClient calls auto unsubscribe the observable. It means there
+     *  is no risk of getting into memory leaks due to opn subscription.
+     * */
     this.worldChampionService.get().subscribe(data => {
-      console.log('data', data);
+      // Extract fields from array and use to rebuild a new desired array suitable for the presentation layer.
       data.MRData.StandingsTable.StandingsLists.forEach(item => {
         champions.push({
           season: item.season,
@@ -25,5 +34,4 @@ export class WorldChampionFacade {
     });
     return of(champions);
   }
-
 }
