d599b7e68e9170dd7a5f7c836757dcd72e63e89f
Henry Oforeh <henryoforeh@gmail.com>
2019-09-29 05:48:20 +0200

    refactor interceptor to retry and handle error

--- src/app/core/interceptors/http-client.interceptor.ts
+++ src/app/core/interceptors/http-client.interceptor.ts
@@ -1,22 +1,31 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent
-} from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
+import { retry, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs/internal/observable/throwError';
 
+// this intercepts every http request and also an error received from the end point.
+// It also the ideal layer to inject tokens, etc in the header.
 @Injectable()
 export class HttpClientInterceptor implements HttpInterceptor {
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    const headersConfig = {
-      Accept: 'application/json'
-    };
-    const request = req.clone({ setHeaders: headersConfig });
-    return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const customReq = request.clone({
+        headers: request.headers.set('Accept', 'application/json')
+    });
+    return next.handle(customReq)
+    .pipe(retry(1),
+    catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          // client-side error
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          // server-side error
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        console.log('server error', errorMessage);
+        return throwError(errorMessage);
+      })
+    );
   }
 }
