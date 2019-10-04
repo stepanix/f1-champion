667910b797e8a42146690af0a9b7e6e5b0e59483
Henry Oforeh <henryoforeh@gmail.com>
2019-10-04 05:15:25 +0200

    clean up css, add favicon, add background

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DriverNamePipe } from './pipes/driver/driver-name.pipe';
 import { LoadingIndicatorComponent } from './components/loading-indicator/views/loading-indicator.component';
 import { ErrorScreenComponent } from './components/error-screen/views/error-screen.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [DriverNamePipe, LoadingIndicatorComponent, ErrorScreenComponent],
-  exports: [DriverNamePipe, LoadingIndicatorComponent, ErrorScreenComponent],
+  declarations: [DriverNamePipe, LoadingIndicatorComponent, ErrorScreenComponent, HeaderComponent],
+  exports: [DriverNamePipe, LoadingIndicatorComponent, ErrorScreenComponent, HeaderComponent],
   imports: [
     CommonModule
   ]
